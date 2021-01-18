409b3e1f5c3cedca9309a2bedb3f9080a480f3c3
Vladimir Kilinich <kylynych.v@gmail.com>
2021-01-18 13:53:25 +0200

    рефакторинг js

--- src/js/dark-mode.js
+++ src/js/dark-mode.js
@@ -3,33 +3,38 @@ const Theme = {
     DARK: 'dark-theme',
 };
 
-let darkMode = localStorage.getItem('darkMode');
+const theme = localStorage.getItem('theme');
 
 const menuRef = document.querySelector('.js-menu');
 const sourceRef = document.querySelector('#template');
 const themeImputRef = document.querySelector('#theme-switch-toggle');
 
-themeImputRef.addEventListener('change', () => {
-    if (event.target.checked === true) {
+themeImputRef.addEventListener('change', onThemeToggle);
+
+function onThemeToggle(event) {
+    if (event.target.checked) {
         enableDarkMode();
     } else {
         disableDarkMode();
     }
-});
+}
 
 const enableDarkMode = () => {
     document.body.classList.remove(Theme.LIGHT);
     document.body.classList.add(Theme.DARK);
-    localStorage.setItem('darkMode', 'enabled');
+    localStorage.setItem('theme', 'dark-theme');
 }
 
 const disableDarkMode = () => {
     document.body.classList.remove(Theme.DARK);
     document.body.classList.add(Theme.LIGHT);
-    localStorage.setItem('darkMode', null);
+    localStorage.setItem('theme', 'light-theme');
 }
 
-if (darkMode === 'enabled') {
+if (theme === 'dark-theme') {
     themeImputRef.checked = true;
     enableDarkMode();
+} else {
+    themeImputRef.checked = false;
+    disableDarkMode();
 }
